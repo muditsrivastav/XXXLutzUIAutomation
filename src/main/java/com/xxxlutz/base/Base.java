d724a0791c63c2c07ee4b821bd0b785a345485c0
Mudit Srivastav <mudit.srivastav19@gmail.com>
2020-06-08 00:09:39 +0200

    Added BrowserStack Support

--- src/main/java/com/xxxlutz/base/Base.java
+++ src/main/java/com/xxxlutz/base/Base.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -12,6 +14,8 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Base {
 	
@@ -19,6 +23,7 @@ public class Base {
 	public Properties prop;
 	
 	public WebDriver initializeDriver() {
+		
 		
 		prop = new Properties();
 		FileInputStream fis = null;
@@ -42,6 +47,25 @@ public class Base {
 		case "firefox":
 			break;
 		case "safari":
+			break;
+		case "BrowserStack":
+			//for BrowserStack Integration
+			final String USERNAME = prop.getProperty("userName");
+			final String AUTOMATE_KEY = prop.getProperty("automateKey");
+			final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+			
+			DesiredCapabilities caps = new DesiredCapabilities();
+	        
+			caps.setCapability("os", prop.getProperty("os"));
+			caps.setCapability("os_version", prop.getProperty("os_version"));
+			caps.setCapability("browser", prop.getProperty("browser_name"));
+			caps.setCapability("browser_version", prop.getProperty("browser_version"));
+			caps.setCapability("name", "XXXLutz UI Test");
+			try {
+				driver = new RemoteWebDriver(new URL(URL), caps);
+			} catch (MalformedURLException e1) {
+				e1.printStackTrace();
+			}
 			break;
 		default:
 			break;
